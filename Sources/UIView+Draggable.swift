53141502d093510521cc5617fa41fd665ce84d63
campha10x <campha10x@yahoo.com.vn>
2021-04-09 15:38:08 +0700

    Update UIView+Draggable.swift
    update dragging float button

--- Sources/UIView+Draggable.swift
+++ Sources/UIView+Draggable.swift
@@ -16,8 +16,53 @@ extension UIView {
     
     @objc private func draggedAction(_ pan:UIPanGestureRecognizer){
         
-        let translation = pan.translation(in: self.superview)
-        self.center = CGPoint(x: self.center.x + translation.x, y: self.center.y + translation.y)
-        pan.setTranslation(CGPoint.zero, in: self.superview)
+        let padding = UIScreen.main.bounds.width / 320 * 20
+        switch gesture.state {
+        case .ended:
+            let translation = gesture.translation(in: self.superview)
+            let view = self as! Floaty
+            var x: CGFloat = 0
+            if self.frame.minX > superview!.bounds.width / 2 {
+                x =  superview!.bounds.width - padding - view.size
+            } else {
+                x =  padding
+            }
+            
+            var y: CGFloat = 0
+            if self.frame.minY > superview!.bounds.height - view.size  {
+                var verticalMargin = view.size
+                var horizontalMargin = view.size
+                if #available(iOS 11, *), view.relativeToSafeArea, let safeAreaInsets = UIApplication.shared.delegate?.window??.safeAreaInsets {
+                  verticalMargin += safeAreaInsets.bottom
+                  horizontalMargin += safeAreaInsets.right
+                }
+                x = (superview!.bounds.size.width - horizontalMargin) - view.paddingX
+                y =  (UIScreen.main.bounds.size.height - verticalMargin) - view.paddingY
+            } else if  self.frame.minY > superview!.bounds.height / 2{
+                y =  self.frame.minY + translation.y
+            } else {
+                y =  superview!.bounds.height / 2
+            }
+            
+            let finalPoint = CGPoint(
+                x: x,
+              y: y
+            )
+            UIView.animate(
+                withDuration: 0.2,
+              delay: 0,
+              options: .curveEaseOut,
+              animations: {
+                self.frame.origin = finalPoint
+            })
+        
+        
+        case .changed:
+            let translation = gesture.translation(in: self.superview)
+            self.center = CGPoint(x: self.center.x + translation.x, y: self.center.y + translation.y)
+            gesture.setTranslation(CGPoint.zero, in: self.superview)
+        default:
+            break
+        }
     }
 }
